f2d15eb3c5d53c6ab3cbcbfd93cce58656286f23
Jeffry Gonzalez <jeff@hypertheory.com>
2020-08-27 11:09:12 -0400

    Before Cutoff Refactor

---
+++ BankingDomain/CutoffBonusCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class CutoffBonusCalculator : ICalculateBankAccountBonuses
+    {
+        decimal ICalculateBankAccountBonuses.GetDepositBonusFor(decimal balance, decimal amountToDeposit)
+        {
+            if (BeforeCutoff())
+            {
+                return amountToDeposit * .10M;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        protected virtual bool BeforeCutoff()
+        {
+            return DateTime.Now.Hour <= 17;
+        }
+    }
+}
---
+++ BankingTests/CutoffBonusCalculatorShould.cs
@@ -0,0 +1,46 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class CutoffBonusCalculatorShould
+    {
+        [Fact]
+        public void GiveYouTheBonusBeforeDailyCutoff()
+        {
+            ICalculateBankAccountBonuses calculator = new TestingBonusCalculator(true);
+
+            var bonus = calculator.GetDepositBonusFor(1000, 100);
+
+            Assert.Equal(10M, bonus);
+        }
+
+        [Fact]
+        public void GiveNoBonusAfterDailyCutoff()
+        {
+            ICalculateBankAccountBonuses calculator = new TestingBonusCalculator(false);
+
+            var bonus = calculator.GetDepositBonusFor(1000, 100);
+
+            Assert.Equal(0M, bonus);
+        }
+    }
+
+    public class TestingBonusCalculator : CutoffBonusCalculator
+    {
+        bool beforeCutoffResponse;
+
+        public TestingBonusCalculator(bool beforeCutoffResponse)
+        {
+            this.beforeCutoffResponse = beforeCutoffResponse;
+        }
+
+        protected override bool BeforeCutoff()
+        {
+            return beforeCutoffResponse;
+        }
+    }
+}
